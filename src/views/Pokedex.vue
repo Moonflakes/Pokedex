ea2bfc9d6834cf9efd2a92acdc37a917e8c8bb67
thiery margot <margot.thiery@gmail.com>
2021-02-16 12:19:44 +0100

    delete unusable

--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -16,39 +16,6 @@
         :key="index"
         >
           <pokemon-card :pokemon="pokemon"></pokemon-card>
-          <!-- <v-card
-            class="mx-auto"
-            max-width="344"
-            outlined
-          >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <h4>
-                  {{pokemon.name}}
-                </h4>
-                <v-list-item-subtitle>
-                  id: {{pokemon.id}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  type: {{pokemon.types[0].type.name}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  height: {{pokemon.height}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  weight: {{pokemon.weight}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-                  abilities: {{pokemon.abilities[0].ability.name}}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-
-              <v-list-item-avatar
-              ><img
-                :src="pokemon.sprites.front_default"
-              ></v-list-item-avatar>
-            </v-list-item>
-          </v-card> -->
     </div>
     
   </div>
@@ -80,7 +47,6 @@ export default {
   },
   methods: {
     pokemonSort(by) {
-      console.log(this.pokemons)
       if (by === "id")
         this.pokemons.sort((a, b) => a.id - b.id);
       if (by === "name")
